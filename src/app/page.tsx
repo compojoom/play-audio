d70ef271780e75ad38332ecd11cd8743f8046558
Daniel Dimitrov <daniel@compojoom.com>
2023-10-24 17:02:38 +0200

    improve ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@
 import {useRef, useState} from "react";
 
 const audioFiles = [
-    'Jingle-copper-bell-ding-2.mp3',
-    'Jingle-guitar-riff.mp3',
     'magical-sound-effect.mp3',
+    'Jingle-copper-bell-ding-2.mp3',
+    'Jingle-guitar-riff.mp3'
 ]
 
 import {Fragment} from 'react'
@@ -88,10 +88,10 @@ export default function Home() {
         }
     }
     return (
-        <main className="flex min-h-screen flex-col items-center justify-end p-24">
+        <main className="flex min-h-screen flex-col items-center justify-end p-8">
 
             <div className="relative flex flex-col items-center">
-                <div className={"mb-5"}>
+                <div className={"mb-16"}>
                     <SoundFiles onSelect={(file) => {
 
                         setAudioFile(file)
@@ -100,10 +100,9 @@ export default function Home() {
                         setPlaying(false)
                     }}/>
                 </div>
-                <div>
-
+                <div className={"mb-16"}>
                     <button onClick={play}
-                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-2xl w-64 h-16">
+                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-4xl w-72 h-16">
                         {playing ? 'Stop' : 'Play'} sound
                     </button>
                     <audio ref={audioRef} src={audioFile} onEnded={() => setPlaying(false)} />
